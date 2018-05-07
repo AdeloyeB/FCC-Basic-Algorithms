00d6aa6ebac84a8f01897930962df62ced495836
Bode Adeloye <adeloye.bode@gmail.com>
2018-05-07 12:41:44 +0100

    Chunky Monkey Done

--- 10_chunky_monkey.js
+++ 10_chunky_monkey.js
@@ -1,7 +1,13 @@
 function chunkArrayInGroups(arr, size) {
   // Break it up.
-  return arr;
+  var chunkArray = [];
+
+  for (var i = 0; i < arr.length; i += size) {
+    var Array = arr.slice(i, i + size);
+    chunkArray.push(Array);
+  }
+  console.log(chunkArray);
+  return chunkArray;
 }
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
- 
